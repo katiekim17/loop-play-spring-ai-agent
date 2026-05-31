5d1a654dda282ecedc27ea2d17f5201f9af43b16
katiekim17 <pyeongsuk.kim88@gmail.com>
2026-06-01 01:09:20 +0900

    chore : 라운드3 1단계까지

--- src/main/java/com/baedal/support/OrderItem.java
+++
@@ -1,3 +0,0 @@
-package com.baedal.support;
-
-public record OrderItem(String menuName, int quantity, int unitPrice) {}
--- src/main/java/com/baedal/support/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package com.baedal.support;
-
-public enum OrderStatus {
-    CREATED, ACCEPTED, COOKING, DELIVERING, DELIVERED, CANCELED
-}
